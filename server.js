d67941b581ac4d8de6d6154290ee2177d0ec237f
Taylor Page <jonathantaylorpage@gmail.com>
2018-10-21 22:09:51 -0700

    (feat) Connect static files
    - Connect static files using express middleware connectors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require( 'express' );
 var app = express();
 var path = require( 'path' );
 
+app.use( '/', express.static(__dirname + '/demo'));
 app.get( '/', function( req, res ) {
   res.sendFile( path.join( __dirname + '/demo/demo.html' ));
 });
