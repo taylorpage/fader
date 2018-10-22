bb564125a28192fb590aa112a2944ddde04f51cd
Taylor Page <jonathantaylorpage@gmail.com>
2018-10-21 22:12:47 -0700

    (chore) Move demo to src directory

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require( 'express' );
 var app = express();
 var path = require( 'path' );
 
-app.use( '/', express.static(__dirname + '/demo'));
+app.use( '/', express.static(__dirname + '/src/demo'));
 app.get( '/', function( req, res ) {
-  res.sendFile( path.join( __dirname + '/demo/demo.html' ));
+  res.sendFile( path.join( __dirname + '/src/demo/demo.html' ));
 });
 
 app.listen( 8000 );
